ee7d52a599f5ac3a9f620b8ff9e6ca77df2e7a07
Mrsp137 <131974059+Mrsp137@users.noreply.github.com>
2024-06-24 02:30:49 +0330

    Update Worker.js

--- Worker.js
+++ Worker.js
@@ -3,14 +3,12 @@ addEventListener("fetch", (event) => {
 });
 
 async function handleRequest(request) {
-  // Define the text you want to return . One link per line.
+
   const text = `
 wireguard://yKDSNFf%2BTxD4UbMiw0Uk2%2BJyozED44wotB1H%2FoWD9lY%3D@188.114.99.40:942?address=172.16.0.2%2F32%2C2606%3A4700%3A110%3A8332%3Abbef%3A2f2e%3Af46d%3Ab883%2F128&reserved=109%2C236%2C136&publickey=bmXOC%2BF1FxEMF9dyiK2H5%2F1SUtzH0JuVo51h2wPfgyo%3D&mtu=1280#Warp+1+-+%40FreeV2rng
 wireguard://yKDSNFf%2BTxD4UbMiw0Uk2%2BJyozED44wotB1H%2FoWD9lY%3D@162.159.192.111:7103?address=172.16.0.2%2F32%2C2606%3A4700%3A110%3A8332%3Abbef%3A2f2e%3Af46d%3Ab883%2F128&reserved=109%2C236%2C136&publickey=bmXOC%2BF1FxEMF9dyiK2H5%2F1SUtzH0JuVo51h2wPfgyo%3D&mtu=1280#Warp+2+-+%40FreeV2rng
-
 `;
-
-  // Return the text as a response
+  
   return new Response(text, {
     headers: { "content-type": "text/plain" },
   });
